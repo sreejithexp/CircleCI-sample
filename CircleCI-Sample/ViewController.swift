a119bd0b52b667ec6646d81e9107b8d66b1c5bdc
Aravind K Nair <aravind.nair@experionglobal.com>
2019-11-15 16:04:50 +0530

    test com it

--- CircleCI-Sample/ViewController.swift
+++ CircleCI-Sample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+			print("its time to st up circle ci")
         // Do any additional setup after loading the view.
     }
 
